d9f86ae1165d864856734b3536f8f4575dbc8ffb
danbka <danbka6126@gmail.com>
2024-08-26 10:07:18 +0700

    ещё 1 тест с избавлением от ассинхронки

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,24 @@
-import { promises as fs } from "fs";
-import path from "path";
+// import { promises as fs } from "fs";
+// import path from "path";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = () => {
-  const packageJsonPath = path.join(process.cwd(), "package.json");
+  // const packageJsonPath = path.join(process.cwd(), "package.json");
 
   let packageJson = {};
-  try {
-    packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
-  } catch (error) {
-    console.error("Error reading package.json:", error);
-  }
+  // try {
+  //   packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
+  // } catch (error) {
+  //   console.error("Error reading package.json:", error);
+  // }
 
   return {
     compiler: {
       styledComponents: true,
     },
     env: {
-      APP_VERSION: packageJson.version,
+      // APP_VERSION: packageJson.version,
+      APP_VERSION: "0.0.1",
     },
     images: {
       unoptimized: true,
@@ -25,8 +26,8 @@ const nextConfig = () => {
     reactStrictMode: true,
 
     // output: "export",
-    basePath: "/cube",
-    assetPrefix: "/cube/",
+    // basePath: "/cube",
+    // assetPrefix: "/cube/",
   };
 };
 
